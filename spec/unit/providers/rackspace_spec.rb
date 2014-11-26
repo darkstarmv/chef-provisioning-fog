d9781a083779a1ba3cb2f5b1623aa0ebb162d2aa
Peter Burkholder <peterb@getchef.com>
2014-11-25 21:57:36 -0500

    Passes Rackspace spec

--- spec/unit/providers/rackspace_spec.rb
+++ spec/unit/providers/rackspace_spec.rb
@@ -4,17 +4,20 @@ require 'chef/provisioning/fog_driver/providers/rackspace'
 describe Chef::Provisioning::FogDriver::Providers::Rackspace do
   subject { Chef::Provisioning::FogDriver::Driver.from_provider(
   	'Rackspace',{
-  		  rackspace_username: 'foo',
-  		  rackspace_api_key: 'bar'
-  		}) }
+  		driver_options: {
+  			compute_options: {
+	  		  rackspace_username: 'foo',
+	  		  rackspace_api_key: 'bar'
+	  		}
+	  	}
+		}) }
 
   it "returns the correct driver" do
     expect(subject).to be_an_instance_of Chef::Provisioning::FogDriver::Providers::Rackspace
   end
 
   it "has a fog backend" do
-    pending unless Fog.mock
-    binding.pry
+    pending unless Fog.mock?
     expect(subject.compute).to be_an_instance_of Fog::Compute::RackspaceV2::Mock
   end
 
