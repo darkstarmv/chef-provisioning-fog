772d37148598be0929d9c347e1070bbf1dec4a36
Peter Burkholder <peterb@getchef.com>
2014-12-04 11:38:26 -0500

    Moved default machine opts to driver

--- examples/cookbooks/fog/recipes/gce.rb
+++ examples/cookbooks/fog/recipes/gce.rb
@@ -8,30 +8,40 @@ with_driver(
 	compute_options: {
 		google_project: node['fog']['google_project'],
 		google_client_email: node['fog']['google_client_email'],
-		google_key_location: node['fog']['google_key_location']
+		google_key_location: node['fog']['google_key_location'],
+		# zone_name: "us-central1-a" # not sure if this is really necessary
 	}
 )
 
-with_machine_options(
-:bootstrap_options => {
-  disk_options: { 
-	  source_image: 'chef-base-debian-7-wheezy-20141119',
-	  description: 'default source_image',
-	  size_gb: 10,
-	  name: 'new-resource-name',
-	  zone_name: 'us-central1-a'
-	},
-	name: "test-1",
-	tags: {tag: "tag1"},
-	zone_name: "us-central1-a",
-  machine_type: "n1-standard-1",
-  network_name: "default",
-	service_account_scopes: ["compute", "userinfo.email", "devstorage.full_control"],
-	auto_restart: true,
-	on_host_maintenance: "MIGRATE",
-} )
+with_machine_options({})
 
 machine 'mario' do
   tag 'itsa_me'
   converge true
 end
+
+##
+## Some other options one may pass to disk 
+## in a future iteration:
+## 
+# with_machine_options(
+# :bootstrap_options => {
+# 	# disk_options does not take a name
+#   disk_options: { 
+# 	  source_image: 'chef-base-debian-7-wheezy-20141119',
+# 	  size_gb: 10,
+# 	  zone_name: 'us-central1-a',
+# 	},
+# 	# disks is an array of 
+# 	disks: [
+# 		# if `disks: []` is empty or non-existent 
+# 		# create one boot_disk with disk_options and named for the instance itself
+# 		# using default_disk_options if no disk_options specified
+# 		boot_disk: {}, # boot_disk is shorthand for a boot-disk named for the instance itself
+# 		special_data_disk: {}, # create or reattach 'special_data_disk' with disk_options
+# 		log_disk: {
+# 			size_gb: 100 # create or reattach with specifed options.
+# 		}
+# 	]
+# } )
+# 
--- lib/chef/provisioning/fog_driver/providers/google.rb
+++ lib/chef/provisioning/fog_driver/providers/google.rb
@@ -1,4 +1,5 @@
-# fog:Rackspace:https://identity.api.rackspacecloud.com/v2.0
+## ToDo:
+##   - accept zone_name as a attribute for machine and disk
 class Chef
 module Provisioning
 module FogDriver
@@ -11,14 +12,86 @@ module FogDriver
         compute_options[:google_client_email]
       end
 
-      def create_servers(action_handler, specs_and_options, parallelizer)
+      def self.default_disk_options()
+        options = {
+          source_image: 'debian-7-wheezy-v20141108',
+          zone_name: 'us-central1-a',
+          size_gb: 10,
+          wait_for: false,
+          boot: false,
+          writable: true,
+          auto_delete: true,
+          timeout: 60,
+          ignore_exists: true, # used locally, not a GCE/Fog thing
+        }
+      end
+      
+      def self.disk_options(options={})
+        # options.merge(default_disk_options)
+        default_disk_options().merge(options)
+      end
 
+      def self.default_bootstrap_options()
+        options = {
+          wait_for: false,
+          zone_name: "us-central1-a",
+          machine_type: "n1-standard-1",
+          network_name: "default",
+          service_account_scopes: ["compute", "userinfo.email", "devstorage.full_control"],
+          auto_restart: true,
+          on_host_maintenance: "MIGRATE",
+          disks: [],
+          disk_options: disk_options,
+        }
+      end
+
+      def self.compute_options_for(provider, id, config)
+        new_compute_options = {}
+        new_compute_options[:provider] = provider
+        new_config = { :driver_options => { :compute_options => new_compute_options }}
+        bootstrap_options = default_bootstrap_options
+        new_defaults = {
+          :driver_options => { :compute_options => {} },
+          :machine_options => { :bootstrap_options => bootstrap_options }
+        }
+        result = Cheffish::MergedConfig.new(new_config, config, new_defaults)
+
+        new_compute_options[:google_project] = id if (id && id != '')
+        credential = Fog.credentials
+
+        new_compute_options[:google_project] ||= credential[:google_project]
+        new_compute_options[:google_client_email] ||= credential[:google_client_email]
+        new_compute_options[:google_key_location] ||= credential[:google_key_location]
+    
+        id = result[:driver_options][:compute_options][:google_project]
+        binding.pry
+        [result, id]
+      end
+
+      def create_disk(options)
+        # determine how to search if disk exists.
+        disk = compute.disks.get(options[:name]) || 
+          compute.disks.create(options)
+        disk.wait_for { disk.ready? } if options[:wait_for]
+        disk.reload
+      end
+
+      def create_servers(action_handler, specs_and_options, parallelizer)
+        # provision and associate disks
         specs_and_options.each do |machine_spec, machine_options|
           bootstrap_options = bootstrap_options_for(action_handler, machine_spec, machine_options)
-          boot_disk = compute.disks.create(bootstrap_options[:disk_options])
-          binding.pry
+          if bootstrap_options[:disks].empty? 
+            disk_options = bootstrap_options[:disk_options].dup 
+            disk_options[:boot] = true
+            disk_options[:name] = machine_spec.node["name"]
+            boot_disk = create_disk(disk_options)
+            machine_options[:bootstrap_options][:disks] << boot_disk
+          else
+            raise ArgumentError, "disk array not supported yet"
+          end
         end
 
+        # can I do a server_for here?
         super(action_handler, specs_and_options, parallelizer) do |machine_spec, server|
           yield machine_spec, server if block_given?
           binding.pry
@@ -26,27 +99,6 @@ module FogDriver
           bootstrap_options = symbolize_keys(machine_options[:bootstrap_options] || {})
         end
       end
-
-      def self.compute_options_for(provider, id, config)
-        new_compute_options = {}
-        new_compute_options[:provider] = provider
-        new_config = { :driver_options => { :compute_options => new_compute_options }}
-        new_defaults = {
-          :driver_options => { :compute_options => {} },
-          :machine_options => { :bootstrap_options => {} }
-        }
-        result = Cheffish::MergedConfig.new(new_config, config, new_defaults)
-
-        credential = Fog.credentials
-
-        new_compute_options[:google_project] ||= credential[:google_project]
-        new_compute_options[:google_client_email] ||= credential[:google_client_email]
-        new_compute_options[:google_key_location] ||= credential[:google_key_location]
-
-        id = "result[:driver_options][:compute_options][:google_project]:#{result[:driver_options][:compute_options][:zone_name]}"
-        [result, id]
-      end
-
     end
   end
 end
